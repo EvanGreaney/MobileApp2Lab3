4dd6f5f2e111ef2c31b2a8382bf96889277e474f
EvanGreaney <g00348451@gmit.ie>
2019-02-05 17:26:22 +0000

    Second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'root-directive',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'MobileApp2Lab3';
+  name = 'Evan';
+  age = '21';
 }
